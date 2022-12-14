eb3aee4e1203af2f00652b50d1df3dcb2c854a8e
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-12-15 02:40:23 +0300

    feat: add foreach items class, change css for open class

--- pages/main/index.js
+++ pages/main/index.js
@@ -1,16 +1,20 @@
 const burgerMenu = document.querySelector('.hamburger-menu');
 const list = document.querySelector('.header .list');
 const nav = document.querySelector('.nav');
-const navList = nav.cloneNode(1);
+const navList = list.cloneNode(1);
+navList.children[0].classList.remove('active-link')
+const links = navList.querySelectorAll('.nav-link')
 const logo = document.querySelector('.header .logo');
 const modal = document.querySelector('.modal');
 
 burgerMenu.addEventListener('click', () => {
   burgerMenu.classList.toggle('active');
   modal.classList.remove('hide');
-
   navList.prepend(logo);
   navList.classList.add('open');
+links.forEach(element => {
+  element.classList.add('open')
+});
   modal.append(navList);
 
 
