8b13533e8cf02dd0a172caaf7c998e68071ce2e3
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-12-15 00:34:30 +0300

    feat: add listener for burger menu

---
+++ pages/main/index.js
@@ -0,0 +1,11 @@
+const burgerMenu = document.querySelector('.hamburger-menu');
+const list = document.querySelector('.list');
+const navLinks = document.querySelectorAll('.nav-item');
+const nav = document.querySelector('.nav');
+
+burgerMenu.addEventListener('click', () => {
+  console.log(burgerMenu)
+  burgerMenu.classList.toggle('active');
+  list.classList.toggle("change");
+  nav.classList.toggle('active');
+});
