3ad6dc1c904b491b62abac5f309bfc1544da8ec7
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-12-15 01:50:00 +0300

    feat: change lsitener handler, and add open class for nav

--- pages/main/index.js
+++ pages/main/index.js
@@ -1,14 +1,18 @@
 const burgerMenu = document.querySelector('.hamburger-menu');
-const list = document.querySelector('.list');
-const navLinks = document.querySelectorAll('.nav-item');
+const list = document.querySelector('.header .list');
 const nav = document.querySelector('.nav');
+const navList = nav.cloneNode(1);
+const logo = document.querySelector('.header .logo');
 const modal = document.querySelector('.modal');
 
 burgerMenu.addEventListener('click', () => {
-  console.log(burgerMenu)
   burgerMenu.classList.toggle('active');
-  list.classList.toggle("change");
-  nav.classList.toggle('active');
   modal.classList.remove('hide');
-  
+
+  navList.prepend(logo);
+  navList.classList.add('open');
+  modal.append(navList);
+
+
+
 });
