83182741563ac32fa3a7fb72ac6e5a8061f7438f
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-12-15 00:50:43 +0300

    feat: add modal div, styles

--- pages/main/index.js
+++ pages/main/index.js
@@ -2,10 +2,13 @@ const burgerMenu = document.querySelector('.hamburger-menu');
 const list = document.querySelector('.list');
 const navLinks = document.querySelectorAll('.nav-item');
 const nav = document.querySelector('.nav');
+const modal = document.querySelector('.modal');
 
 burgerMenu.addEventListener('click', () => {
   console.log(burgerMenu)
   burgerMenu.classList.toggle('active');
   list.classList.toggle("change");
   nav.classList.toggle('active');
+  modal.classList.remove('hide');
+  
 });
