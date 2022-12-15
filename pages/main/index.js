acf791e893a10e4a40dfdfc24a16b50383c8694c
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-12-15 12:06:41 +0300

    feat: change img src in logo

--- pages/main/index.js
+++ pages/main/index.js
@@ -4,18 +4,22 @@ const nav = document.querySelector('.nav');
 const navList = list.cloneNode(1);
 navList.children[0].classList.remove('active-link')
 const links = navList.querySelectorAll('.nav-link')
-const logo = document.querySelector('.header .logo');
+const logoImg = document.querySelector('.header .logo img');
 const modal = document.querySelector('.modal');
 
 burgerMenu.addEventListener('click', () => {
   burgerMenu.classList.toggle('active');
-  modal.classList.remove('hide');
-  navList.prepend(logo);
+  modal.classList.toggle('hide');
+  navList.prepend(logoImg);
   navList.classList.add('open');
 links.forEach(element => {
   element.classList.add('open')
 });
   modal.append(navList);
+ if (burgerMenu.classList.contains('active')){
+  document.querySelector('.header-wrapper').style.justifyContent = 'end';
+  logoImg.src='../../assets/icons/logo_modal.svg'
+ } 
 
 
 
