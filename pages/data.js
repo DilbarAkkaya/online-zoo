1b9a7d81994c325ad0c96fe9c97921bf3e1051ce
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-12-16 10:48:53 +0300

    feat: add all pets info to data.js

---
+++ pages/data.js
@@ -0,0 +1,58 @@
+const data = [
+  {
+    id: 1,
+    petName: 'Alligators',
+    image: '../../assets/images/alligator.jpg',
+    location: 'Native to Southwest China',
+    imageFood: '../../assets/icons/meet-fish_icon.svg',
+  },
+  {
+    id: 2,
+    petName: 'Eagle',
+    image: '../../assets/images/eagle.jpg',
+    location: 'Native to South America',
+    imageFood: '../../assets/icons/meet-fish_icon.svg',
+  },
+  {
+    id: 3,
+    petName: 'Gorillas',
+    image: '../../assets/images/goril1.jpg',
+    location: 'Native to Congo',
+    imageFood: '../../assets/icons/banana-bamboo_icon.svg',
+  },
+  {
+    id: 4,
+    petName: 'Two-toed Sloth',
+    image: '../../assets/images/leav.jpg',
+    location: 'Mesoamerica, South America',
+    imageFood: '../../assets/icons/banana-bamboo_icon.svg',
+  },
+  {
+    id: 5,
+    petName: 'Cheetahs',
+    image: '../../assets/images/leo.jpg',
+    location: 'Native to Africa',
+    imageFood: '../../assets/icons/meet-fish_icon.svg',
+  },
+  {
+    id: 6,
+    petName: 'Penguins',
+    image: '../../assets/icons/meet-fish_icon.svg',
+    location: 'Native to Antarctica',
+    imageFood: '../../assets/icons/meet-fish_icon.svg',
+  },
+  {
+    id: 7,
+    petName: 'Giant pandas',
+    image: '../../assets/images/pandas.jpg',
+    location: 'Native to Southwest China',
+    imageFood: '../../assets/icons/banana-bamboo_icon.svg',
+  },
+  {
+    id: 8,
+    petName: 'Gorillas',
+    image: '../../assets/images/goril.jpg',
+    location: 'Native to Congo',
+    imageFood: '../../assets/icons/banana-bamboo_icon.svg',
+  },
+]
