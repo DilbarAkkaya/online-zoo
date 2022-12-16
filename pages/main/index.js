73d16683071ae4c24fd24ad3eb85430ba1517e8d
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-12-17 00:20:50 +0300

    refactor: delete comments, consolelogs, unused variables

--- pages/main/index.js
+++ pages/main/index.js
@@ -1,7 +1,6 @@
 import data from '../data.js';
 const burgerMenu = document.querySelector('.hamburger-menu');
 const list = document.querySelector('.header .list');
-const nav = document.querySelector('.nav');
 const navList = list.cloneNode(1);
 navList.children[0].classList.remove('active-link')
 const links = navList.querySelectorAll('.nav-link')
@@ -35,62 +34,50 @@ const sliderLeft = document.querySelector('#slider-left');
 const sliderRight = document.querySelector('#slider-right');
 let sliderCenter = document.querySelector('#slider-center');
 
-const moveLeft = ()=>{
+const moveLeft = () => {
   slideContainer.classList.add('transition-left');
   btnPrev.removeEventListener('click', moveLeft);
   btnNext.removeEventListener('click', moveRight);
 };
 btnPrev.addEventListener('click', moveLeft);
 
-const moveRight = ()=>{
+const moveRight = () => {
   slideContainer.classList.add('transition-right');
   btnPrev.removeEventListener('click', moveLeft);
   btnNext.removeEventListener('click', moveRight);
 };
 btnNext.addEventListener('click', moveRight);
 
-slideContainer.addEventListener('animationend', (event)=>{
-if (event.animationName === 'move-left') {
+slideContainer.addEventListener('animationend', (event) => {
+  if (event.animationName === 'move-left') {
 
-  slideContainer.classList.remove('transition-left');
-  let leftItems = sliderLeft.innerHTML;
-  sliderCenter.innerHTML = leftItems;
- sliderLeft.innerHTML = '';
-//createCard(randomShEl);
-createSliderLeft();
-} else {
-  slideContainer.classList.remove('transition-right');
-  let rightItems = sliderRight.innerHTML;
+    slideContainer.classList.remove('transition-left');
+    let leftItems = sliderLeft.innerHTML;
+    sliderCenter.innerHTML = leftItems;
+    sliderLeft.innerHTML = '';
+    createSliderLeft();
+  } else {
+    slideContainer.classList.remove('transition-right');
+    let rightItems = sliderRight.innerHTML;
 
-  sliderCenter.innerHTML = rightItems;
-  console.log(sliderCenter)
-  sliderRight.innerHTML = '';
-  createSliderRight();
-}
+    sliderCenter.innerHTML = rightItems;
+    sliderRight.innerHTML = '';
+    createSliderRight();
+  }
   btnPrev.addEventListener('click', moveLeft);
   btnNext.addEventListener('click', moveRight);
 });
 
-/* const getRandomEl = (arr) => {
- return arr[Math.floor(Math.random()*arr.length)];
-}  */
-
 const shuffle = (arr) => {
-return arr.sort(()=>Math.random()-0.5)
+  return arr.sort(() => Math.random() - 0.5)
 }
 
-/* const shuffledData = shuffle(data);
-console.log(shuffledData) */
-/* 
-const randomShEl = getRandomEl(shuffledData);
-console.log(randomShEl.petName */
-
-const createCard = (randomShEl)=> {
+const createCard = (randomShEl) => {
   const card = document.createElement('figure');
   card.setAttribute('data-id', `${randomShEl.id}`);
   card.classList.add('card', 'slide');
-  const alt = `${randomShEl.imageFood==='../../assets/icons/meet-fish_icon.svg'? "meatfish" : "banana"}`;
-  const imageClass = `${randomShEl.imageFood==='../../assets/icons/meet-fish_icon.svg'? "meat" : "veget"}`;
+  const alt = `${randomShEl.imageFood === '../../assets/icons/meet-fish_icon.svg' ? "meatfish" : "banana"}`;
+  const imageClass = `${randomShEl.imageFood === '../../assets/icons/meet-fish_icon.svg' ? "meat" : "veget"}`;
   card.insertAdjacentHTML('beforeend', `
   <div class="img-wrap">
                       <img src=${randomShEl.image} alt="alligator" class="card-img">
@@ -104,59 +91,19 @@ const createCard = (randomShEl)=> {
                         <img src=${randomShEl.imageFood} alt=${alt} class=${imageClass}>
                       </div>
                     </figcaption>`);
-return card
+  return card
 }
-const createSliderLeft = ()=>{
+const createSliderLeft = () => {
   const shuffledData = shuffle(data);
-  for(let i=0; i<shuffledData.length-2;i++){
-
-   // const randomShEl = getRandomEl(shuffledData);
-        let leftSliderItem = createCard(shuffledData[i]);
-    // leftArr.push(leftSliderItem);
-    /*    if(!leftArr.includes(leftSliderItem.dataset)){
-         console.log(leftSliderItem.dataset.id) */
-         sliderLeft.appendChild(leftSliderItem)
+  for (let i = 0; i < shuffledData.length - 2; i++) {
+    let leftSliderItem = createCard(shuffledData[i]);
+    sliderLeft.appendChild(leftSliderItem)
   }
 }
-  const createSliderRight = ()=>{
-    const shuffledData = shuffle(data);
-    for(let i=0; i<shuffledData.length-2;i++){
-  
-     // const randomShEl = getRandomEl(shuffledData);
-          let rightSliderItem = createCard(shuffledData[i]);
-          console.log(rightSliderItem)
-      // leftArr.push(leftSliderItem);
-      /*    if(!leftArr.includes(leftSliderItem.dataset)){
-           console.log(leftSliderItem.dataset.id) */
-           sliderRight.appendChild(rightSliderItem)
-    }
- // leftArr=[];
-
-      }
-/* const createCard = (data)=>{
-let randomNum = Math.floor(7*Math.random());
-  const card = document.createElement('figure');
-  card.setAttribute('data-id', `${data[randomNum].id}`);
-  card.classList.add('card', 'slide');
-  card.insertAdjacentHTML('beforeend', `
-  <div class="img-wrap">
-                      <img src=${data[randomNum].image} alt="alligator" class="card-img">
-                    </div>
-                    <figcaption class="description">
-                      <div class="desc-text">
-                        <h5>${data[randomNum].petName}</h5>
-                        <p>${data[randomNum].location}</p>
-                      </div>
-                      <div class="desc-icon">
-                        <img src=${data[randomNum].imageFood} alt="meat" class="meat">
-                      </div>
-                    </figcaption>`);
-return card
-} */
-
-/*     let leftSliderItem = createCard(data);
-    leftArr.push(leftSliderItem);
-      if(!leftArr.includes(leftSliderItem.dataset)){
-        console.log(leftSliderItem.dataset.id)
-        sliderLeft.appendChild(leftSliderItem)
-      } */
+const createSliderRight = () => {
+  const shuffledData = shuffle(data);
+  for (let i = 0; i < shuffledData.length - 2; i++) {
+    let rightSliderItem = createCard(shuffledData[i]);
+    sliderRight.appendChild(rightSliderItem)
+  }
+}
