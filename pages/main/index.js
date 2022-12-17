a201d63c56849bcbe8973b48d9d456f692ea8389
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-12-18 01:57:19 +0300

    separate input handler with func changeinput

--- pages/main/index.js
+++ pages/main/index.js
@@ -113,29 +113,7 @@ console.log(inputRange.value)
 const testimonialsContainer = document.querySelector('.testimonials-cards');
 const mediaQuery = window.matchMedia('(max-width: 1000px)');
 
-inputRange.addEventListener('input', (event)=>{
-  const value = event.target.value;
-  const WIDTH_IN_1600 = 296;
-  const WIDTH_IN_1000 = 320;
-/*   const calc = 'calc(940px + (1160 - 940) * (100vw - 1000px) / (1600 - 1000))'; */
-  switch(value) {
-    case '0': testimonialsContainer.style.left = '0px';
-    break;
-    case '1': testimonialsContainer.style.left = `calc(${-value*WIDTH_IN_1000}px + (${-value*WIDTH_IN_1600} - ${-value*WIDTH_IN_1000}) * (100vw - 1000px) / (1600 - 1000))`;
-    break;
-    case '2': testimonialsContainer.style.left = `calc(${-value*WIDTH_IN_1000}px + (${-value*WIDTH_IN_1600} - ${-value*WIDTH_IN_1000}) * (100vw - 1000px) / (1600 - 1000))`;
-    break;
-    case '3': testimonialsContainer.style.left = `calc(${-value*WIDTH_IN_1000}px + (${-value*WIDTH_IN_1600} - ${-value*WIDTH_IN_1000}) * (100vw - 1000px) / (1600 - 1000))`;
-    break;
-    case '4': testimonialsContainer.style.left =`calc(${-value*WIDTH_IN_1000}px + (${-value*WIDTH_IN_1600} - ${-value*WIDTH_IN_1000}) * (100vw - 1000px) / (1600 - 1000))`;
-    break;
-    case '5': testimonialsContainer.style.left = `calc(${-value*WIDTH_IN_1000}px + (${-value*WIDTH_IN_1600} - ${-value*WIDTH_IN_1000}) * (100vw - 1000px) / (1600 - 1000))`;
-    break;
-    case '6': testimonialsContainer.style.left = `calc(${-value*WIDTH_IN_1000}px + (${-value*WIDTH_IN_1600} - ${-value*WIDTH_IN_1000}) * (100vw - 1000px) / (1600 - 1000))`;
-    break;
-    case '7': testimonialsContainer.style.left = `calc(${-value*WIDTH_IN_1000}px + (${-value*WIDTH_IN_1600} - ${-value*WIDTH_IN_1000}) * (100vw - 1000px) / (1600 - 1000))`;
-    break;
-  }
+inputRange.addEventListener('input',changeInput)
 /* switch(value) {
   case '0': testimonialsContainer.style.left = '0px';
   break;
@@ -174,5 +152,27 @@ inputRange.addEventListener('input', (event)=>{
     break;
 }
 } */
+function changeInput (event) {
+  const value = event.target.value;
+  const WIDTH_IN_1600 = 296;
+  const WIDTH_IN_1000 = 320;
+/*   const calc = 'calc(940px + (1160 - 940) * (100vw - 1000px) / (1600 - 1000))'; */
+  switch(value) {
+    case '0': testimonialsContainer.style.left = '0px';
+    break;
+    case '1': testimonialsContainer.style.left = `calc(${-value*WIDTH_IN_1000}px + (${-value*WIDTH_IN_1600} - ${-value*WIDTH_IN_1000}) * (100vw - 1000px) / (1600 - 1000))`;
+    break;
+    case '2': testimonialsContainer.style.left = `calc(${-value*WIDTH_IN_1000}px + (${-value*WIDTH_IN_1600} - ${-value*WIDTH_IN_1000}) * (100vw - 1000px) / (1600 - 1000))`;
+    break;
+    case '3': testimonialsContainer.style.left = `calc(${-value*WIDTH_IN_1000}px + (${-value*WIDTH_IN_1600} - ${-value*WIDTH_IN_1000}) * (100vw - 1000px) / (1600 - 1000))`;
+    break;
+    case '4': testimonialsContainer.style.left =`calc(${-value*WIDTH_IN_1000}px + (${-value*WIDTH_IN_1600} - ${-value*WIDTH_IN_1000}) * (100vw - 1000px) / (1600 - 1000))`;
+    break;
+    case '5': testimonialsContainer.style.left = `calc(${-value*WIDTH_IN_1000}px + (${-value*WIDTH_IN_1600} - ${-value*WIDTH_IN_1000}) * (100vw - 1000px) / (1600 - 1000))`;
+    break;
+    case '6': testimonialsContainer.style.left = `calc(${-value*WIDTH_IN_1000}px + (${-value*WIDTH_IN_1600} - ${-value*WIDTH_IN_1000}) * (100vw - 1000px) / (1600 - 1000))`;
+    break;
+    case '7': testimonialsContainer.style.left = `calc(${-value*WIDTH_IN_1000}px + (${-value*WIDTH_IN_1600} - ${-value*WIDTH_IN_1000}) * (100vw - 1000px) / (1600 - 1000))`;
+    break;
+  }
 }
-)
